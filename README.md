b39c531e2f7842e95e569d03c4ba6856d73dfed1
person <agdehaan@gmail.com>
2020-05-31 12:25:25 +0200

    Add setup + improve readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Sanic-Sessions
+# Sanic-Sessions 0.1.0
+
+### Why does Sanic-Sessions exist?
+First of all. I need a simple way to handle sessions in sanic with mongoengine.
+The purpose of this repository is mainly to speed up production for my developer exams.
+
+### Will you upload this to PyPi
+Probably not. but maybe when a lot of people use this module.
+
+### Installation
+
+#### With pip and git (default)
+`py -m pip install git+https://github.com/Vepnar/Sanic-Sessions.git`
+
+#### With pip without git
+`py -m pip install https://github.com/Vepnar/Sanic-Sessions/archive/master.zip`
+
+
+### TODO
+- Replace mongoengine with an alternative asynchronous database driver.
+- Add Examples
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="sanic-sessions",
+    version="0.1.0",
+    author="Arjan de Haan",
+    author_email="vepnardev@gmail.com",
+    license="MIT",
+    description="Easy way to handle sessions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Vepnar/Sanic-Sessions",
+    platforms="any",
+    packages=['sanic_authenticate'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=required
+)
